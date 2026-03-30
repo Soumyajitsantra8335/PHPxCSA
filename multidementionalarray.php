d0f3b0d97021028328eca1910a0e4c3f47f71ab4
javascript1414 <soumosantra588@gmail.com>
2026-03-30 17:08:34 +0530

    init:check

--- multidementionalarray.php
+++
@@ -1,9 +0,0 @@
-<?php
-$students = array(
-    array("Soumo", 22, "Kolkata"),
-    array("Rinku", 21, "Delhi"),
-    array("Amit", 23, "Mumbai")
-);
-echo $students[0][0];
-echo $students[1][0];
-?>
